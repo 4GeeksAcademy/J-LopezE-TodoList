d32baf05bdc330bf9d91ed68cd3e9dbf2a6eef02
J-LopezE <163783876+J-LopezE@users.noreply.github.com>
2024-05-28 12:34:58 +0000

    todoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+	const [inpuntTask, setInputTask] = useState("");
+	const [taskList, setTaskList] = useState([])
+
+	const taskAdd = (event) => {
+		setInputTask(event.target.value);
+
+	}
+	const submitTask = () => {
+		setTaskList([...taskList, inpuntTask])
+		setInputTask("")
+	}
+	const evenKey = (event) => {
+		if (event.key === "Enter")
+			return submitTask();
+		else console.log("estas mal")
+	}
+	const deletTask = (index) => {
+		const filterTask = taskList.filter((task, i) => i !== index)
+		setTaskList(filterTask)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card mb-3 mt-5 text-center">
+			<h1>TodoList</h1>
+			 <div className="card-header d-flex">
+				<input type="text" className="form-control" value={inpuntTask} onChange={taskAdd} onKeyDown={evenKey} />
+				<button className="btn btn-primary" onClick={submitTask}>+</button>
+			</div>
+			<div className="card-body">
+					<ul className="list-group">
+						{taskList.map((task, index) => {
+							return <li key={task+index} className="list-group-item active d-flex justify-content-between" aria-current="true">{task}
+								<button className="btn btn-danger" onClick={() => deletTask(index)}>X</button>
+							</li>
+						})}
+					</ul>
+				</div> 
 		</div>
 	);
 };
